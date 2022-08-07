b6f6eb48c2dbcceef2d5ccf479e314eadbaadb40
Sejan Mahmud <sejan157218@gmail.com>
2022-08-07 20:08:44 +0600

    query and or union

---
+++ student/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+from .models import Student,Teacher
+from django.db import connection
+from django.db.models import Q
+
+# Part 2
+#################################################################
+def student_list_(request):
+
+    posts = Student.objects.all()
+
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})
+
+def student_list_(request):
+
+    # posts = Student.objects.filter(surname__startswith='austin') | Student.objects.filter(surname__startswith='baldwin')
+    posts = Student.objects.filter (Q(surname__startswith='austin') | Q (surname__startswith='baldwin'))
+# 
+
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})
+
+
+
+
+def student_list_(request):
+
+    # posts = Student.objects.filter(classroom=3) & Student.objects.filter(surname__startswith='baldwin')
+    posts = Student.objects.filter (Q(classroom=3) & Q (surname__startswith='baldwin'))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})
+
+
+def student_list(request):
+    # posts=Student.objects.all().values_list('firstname').union(Teacher.objects.all().values_list('firstname'))
+    posts=Student.objects.all().values('firstname').union(Teacher.objects.all().values('firstname'))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def student_list_(request):
+#     posts = Student.objects.filter(surname__startswith='austin') | Student.objects.filter(surname__startswith='baldwin')
+
+#     print(posts)
+#     print(connection.queries)
+
+#     return render(request, 'output.html',{'posts':posts})
+
+# def student_list(request):
+#     posts = Student.objects.filter(Q(surname__startswith='austin') | ~Q (surname__startswith='baldwin') | Q (surname__startswith='avery-parker'))
+
+#     print(posts)
+#     print(connection.queries)
+
+#     return render(request, 'output.html',{'posts':posts})
